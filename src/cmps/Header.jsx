e915cf509a224e5638aeeabb1b20547bf4d29468
Bercode257 <berc.david@gmail.com>
2021-09-24 02:04:35 +0300

    css update

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,12 +1,16 @@
-import Logo from '../assets/img/logo.jpg';
+import Logo from '../assets/img/logo.png';
 import { NavLink } from 'react-router-dom';
 
 export function Header() {
     return (
         <header className="header flex align-center justify-between">
-            <NavLink to="/">
-                <img src={Logo} height="40px" />
-            </NavLink>
+
+            <div className="logo">
+                <NavLink to="/">
+                    <img src={Logo} height="40px" />
+                    Wixer
+                </NavLink>
+            </div>
             <nav className="links flex">
                 <div className="link flex align-center">
                     <NavLink to="/editor">Editor</NavLink>
