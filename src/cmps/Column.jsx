e5896d3eb69d5cd45b887f4caad61b3f16dbf253
ori <oripilpelphone@gmail.com>
2021-09-24 16:07:21 +0300

    fixed moving cols between ineersection

--- src/cmps/Column.jsx
+++ src/cmps/Column.jsx
@@ -5,7 +5,7 @@ import { COLUMN, COMPONENT, SIDEBAR_ITEM } from "../constants";
 import { DropZone } from "./DropZone";
 import Component from "./Component";
 
-const Column = ({ data, cmps, handleDrop, path, moveColumn, updateComponent, onSelect, selected }) => {
+const Column = ({ data, cmps, handleDrop, path, updateComponent, onSelect, selected }) => {
   const ref = useRef(null);
   const [{ handlerId }, drop] = useDrop({
     accept: COLUMN,
@@ -13,51 +13,7 @@ const Column = ({ data, cmps, handleDrop, path, moveColumn, updateComponent, onS
       return {
         handlerId: monitor.getHandlerId(),
       };
-    },
-    // hover(item, monitor) {
-    //   if (!ref.current) {
-    //     return;
-    //   }
-    //   // console.log('currPath:', path, 'destPath', item.path)
-    //   const dragPath = path.split('-');
-    //   const currPath = item.path.split('-');
-    //   const dragIndex = (dragPath[0] === currPath[0]) ? item.path.split('-')[1] : -1;
-    //   const hoverIndex = dragPath[1];
-    //   // console.log('currPath:', currPath, 'dragPath', dragPath)
-    //   // Don't replace items with themselves
-    //   if (dragPath === currPath) {
-    //     return;
-    //   }
-    //   // Determine rectangle on screen
-    //   const hoverBoundingRect = ref.current?.getBoundingClientRect();
-    //   // console.log('hoverBoundingRect', hoverBoundingRect);
-    //   // Get horizontal middle
-    //   const hoverMiddleX = (hoverBoundingRect.left) + hoverBoundingRect.width / 2;
-    //   // Determine mouse position
-    //   const clientOffset = monitor.getClientOffset();
-    //   // console.log('clientOffset', clientOffset);
-    //   // Get pixels to the top
-    //   const hoverClientX = clientOffset.x //- hoverBoundingRect.left;
-    //   // console.log('hoverClientX:', hoverClientX, 'hoverMiddleX', hoverMiddleX)
-    //   // Only perform the move when the mouse has crossed half of the items height
-    //   // When dragging downwards, only move when the cursor is below 50%
-    //   // When dragging upwards, only move when the cursor is above 50%
-    //   // Dragging left
-    //   if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
-    //     return;
-    //   }
-    //   // Dragging right
-    //   if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
-    //     return;
-    //   }
-    //   // // Time to actually perform the action
-    //   moveColumn(currPath, dragPath);
-    //   // // Note: we're mutating the monitor item here!
-    //   // // Generally it's better to avoid mutations,
-    //   // // but it's good here for the sake of performance
-    //   // // to avoid expensive index searches.
-    //   item.path = path;
-    // },
+    }
   });
 
   const [{ isDragging }, drag] = useDrag({
