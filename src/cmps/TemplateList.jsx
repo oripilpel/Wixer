ab91b4513b73d66aa2e8e88654dc1f81c186f626
ori <oripilpelphone@gmail.com>
2021-09-26 19:39:31 +0300

    changed some things

--- src/cmps/TemplateList.jsx
+++ src/cmps/TemplateList.jsx
@@ -3,17 +3,18 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import ImageList from '@mui/material/ImageList';
 
 import { TemplatePreview } from './TemplatePreview';
+import { templateService } from '../services/template.service';
 
-export function TemplateList({ templates }) {
+export function TemplateList({ numberOfTemplates }) {
 
-    const getColsByScreenSize = () => {
-        let cols
-        if (useMediaQuery('(max-width:570px)')) cols = 1
-        if (useMediaQuery('(min-width:571px)')) cols = 2
-        if (useMediaQuery('(min-width:800px)')) cols = 3
-        return cols
-    }
-    const cols = getColsByScreenSize()
+    numberOfTemplates = (numberOfTemplates) ? numberOfTemplates : 0
+    const templates = templateService.getAmountOfTemplates(numberOfTemplates)
+
+    let cols
+    if (useMediaQuery('(max-width:570px)')) cols = 1
+    if (useMediaQuery('(min-width:571px)')) cols = 2
+    if (useMediaQuery('(min-width:800px)')) cols = 3
+
     return (
         <div id="templates" className="template-list main-layout">
             <h2>Choose a template and start building now!</h2>
