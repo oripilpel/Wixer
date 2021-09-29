ff29f7ed0111023f009903e285a35c34003c9f88
Bercode257 <berc.david@gmail.com>
2021-09-29 12:22:36 +0300

    icons

--- src/cmps/SideBarItem.jsx
+++ src/cmps/SideBarItem.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useDrag } from "react-dnd";
 
-export function SideBarItem({ data, type }){
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import ViewColumnIcon from '@mui/icons-material/ViewColumn';
+
+export function SideBarItem({ data, type }) {
   const [{ opacity }, drag] = useDrag({
     type,
     item: data,
@@ -10,8 +13,34 @@ export function SideBarItem({ data, type }){
     })
   });
 
+  const icon = () => {
+    console.log(data.component.type);
+    switch (data.component.type) {
+      case 'innersection':
+        return (
+          <div className="icon">
+            <ViewColumnIcon />
+          </div>
+        )
+      case 'column':
+      case 'text':
+      case 'video':
+      case 'image':
+      case 'button':
+        return (
+          <div className="icon">
+            <AddBoxIcon />
+          </div>
+        )
+      default:
+        return <></>
+    }
+
+  }
+
   return (
     <div className="side-bar-item" ref={drag} style={{ opacity }}>
+      {icon()}
       {data.name || data.component.type}
     </div>
   );
