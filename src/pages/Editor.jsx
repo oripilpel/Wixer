bf12bd2ada25460683abd2236fff8c8c19a1cd58
ori <oripilpelphone@gmail.com>
2021-10-04 15:55:13 +0300

    jk

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -143,7 +143,6 @@ function _Editor(
 
     const handleDrop =
         (dropZone, item) => {
-            debugger
             const splitDropZonePath = dropZone.path.split("-");
             const pathToDropZone = splitDropZonePath.slice(0, -1).join("-");
 
