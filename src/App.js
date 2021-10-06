d54466acdb126940c593d0079c50de25d149b61f
Bercode257 <berc.david@gmail.com>
2021-10-07 02:10:21 +0300

    berco

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Editor } from './pages/Editor';
 import { Home } from './pages/Home.jsx';
 import { About } from './pages/About';
 import { Header } from './cmps/Header';
 import React from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch
-} from "react-router-dom";
 import { Templates } from './pages/Templates';
 import { Publish } from './pages/Publish';
 import { Dashboard } from './pages/Dashboard';
@@ -19,8 +15,8 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/signup" component={Login} />
-          <Route path="/login" component={Login} />
+          {/* <Route path="/signup" component={Login} />
+          <Route path="/login" component={Login} /> */}
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/templates" component={Templates} />
           <Route path="/editor/:wapId" component={Editor} />
