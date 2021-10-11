b3274e13797892bebb247873980b338dcaec7e33
ori <oripilpelphone@gmail.com>
2021-10-11 16:55:26 +0300

    save modal rdy

--- src/cmps/publish/Text.jsx
+++ src/cmps/publish/Text.jsx
@@ -1,7 +1,7 @@
 
 export function Text({ data, style }) {
     return (
-        <div className={`text ${(+(style.fontSize.slice(0, style.fontSize.length - 2)) >= 45) ? 'big' : ''}`} style={style}>
+        <div className={`text ${(style.fontSize && +(style.fontSize.slice(0, style.fontSize.length - 2)) >= 45) ? 'big' : ''}`} style={style}>
             {data.txt}
         </div>
     )
--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Button } from "@mui/material";
 import { ErrorMessage, Field, Form, Formik } from 'formik';
@@ -6,47 +7,46 @@ import { TextFieldOutlined } from '../cmps/TextFieldOutlined';
 import { onLogin, onSignup } from '../store/user.actions';
 
 function _LoginSignup(props) {
+    const { path } = props.match;
 
-    const path = props.match.path;
+    const [isLogin, setIsLogin] = useState(!!props.login || path === '/login')
 
     const validate = (values, props) => {
         const errors = {};
-        switch (path) {
-            case '/login':
-                if (!values.username)
-                    errors.username = '*required';
-                if (!values.password)
-                    errors.password = '*required';
-                break;
-            default:
-                if (!values.username)
-                    errors.username = '*required';
-                if (!values.password)
-                    errors.password = '*required';
-                if (!values.fullname)
-                    errors.fullname = '*required';
+        if (isLogin) {
+            if (!values.username)
+                errors.username = '*required';
+            if (!values.password)
+                errors.password = '*required';
+        }
+        else {
+            if (!values.username)
+                errors.username = '*required';
+            if (!values.password)
+                errors.password = '*required';
+            if (!values.fullname)
+                errors.fullname = '*required';
         }
         return errors;
     }
 
     const onSubmit = async (values, { setSubmitting }) => {
         let user;
-        switch (path) {
-            case '/login':
-                user = await props.onLogin(values)
-                if (user) {
-                    setSubmitting(false);
-                    props.history.push('/dashboard');
-                }
-                break;
-            default:
-                user = await props.onSignup(values)
-                if (user) {
-                    setSubmitting(false);
-                    props.history.push('/dashboard');
-                }
-
+        if (isLogin) {
+            user = await props.onLogin(values)
+            if (user) {
+                setSubmitting(false);
+                if (!path.includes('editor')) props.history.push('/dashboard');
+            }
         }
+        else {
+            user = await props.onSignup(values)
+            if (user) {
+                setSubmitting(false);
+                if (!path.includes('editor')) props.history.push('/dashboard');
+            }
+        }
+        if (props.setIsLoggedIn) props.setIsLoggedIn()
     }
 
     const initialValues = {
@@ -57,7 +57,7 @@ function _LoginSignup(props) {
 
     return (
         <div className="login-signup flex direction-column align-center">
-            <h1>{path === '/login' ? 'Login' : 'Signup'}</h1>
+            <h1>{(isLogin) ? 'Login' : 'Signup'}</h1>
             <Formik
                 initialValues={initialValues}
                 validate={validate}
@@ -65,7 +65,7 @@ function _LoginSignup(props) {
             >
                 {({ isSubmitting }) => (
                     <Form>
-                        {path === '/signup' && (
+                        {!isLogin && (
                             <div className="field">
                                 <Field type="text" name="fullname" label="Fullname" as={TextFieldOutlined} />
                                 <ErrorMessage className="error" name="fullname" component="div" />
@@ -87,28 +87,27 @@ function _LoginSignup(props) {
                                 color={'primary'}
                                 type="submit"
                                 disabled={isSubmitting}>
-                                {path === '/login' ? 'Login' : 'Signup'}
+                                {(isLogin) ? 'Login' : 'Signup'}
                             </Button>
                         </div>
                     </Form>
                 )}
             </Formik>
-            {path === '/login' ?
-                (<div className="signup">
-                    Don't have a Wixer account? <Link to='/signup'>Signup</Link>
+            {(isLogin) ?
+                (<div className="signup" onClick={() => { setIsLogin(false) }}>
+                    Don't have a Wixer account? {(!path.includes('editor')) ? <Link to='/signup'>Signup</Link> : <a onClick={() => { setIsLogin(false) }}>Signup</a>}
                 </div>) :
 
-                (<div className="login">
-                    Already have an account? <Link to='/login'>Login</Link>
+                (<div className="login" onClick={() => { setIsLogin(true) }}>
+                    Already have an account? {(!path.includes('editor')) ? <Link to='/login'>Login</Link> : <a onClick={() => { setIsLogin(true) }}>Login</a>}
                 </div>)}
             <div style={{ cursor: 'pointer' }} onClick={async () => {
                 const user = await props.onLogin({
                     username: 'demo',
                     password: 'demo'
                 })
-                if (user) {
-                    props.history.push('/dashboard');
-                }
+                if (!path.includes('editor')) props.history.push('/dashboard');
+                if (props.setIsLoggedIn) props.setIsLoggedIn()
             }}>
                 Login As Demo
             </div>
@@ -121,4 +120,4 @@ const mapDispatchToProps = {
     onSignup
 }
 
-export const LoginSignup = connect(null, mapDispatchToProps)(_LoginSignup)
+export const LoginSignup = connect(null, mapDispatchToProps)(withRouter(_LoginSignup))
